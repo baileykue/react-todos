ffc4bf6d5ba8f677d1597e906fc1ef02b87a7e8c
Bailey Kuebelbeck <bailey.kue@gmail.com>
2022-01-11 14:21:47 -0800

    getting auth page set up on app js, changed auth folder name to remove js

--- src/App.js
+++ src/App.js
@@ -2,14 +2,22 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
 import background from './moon.jpg';
 import Footer from './components/Footer/Footer';
+import Auth from './views/Auth/Auth';
 
 function App() {
+  const [currentUser, setCurrentUser] = useState(getUser());
+
+  const signOutUser = async () => {
+    await signOut();
+    setCurrentUser(null);
+  };
   return (
     <div className="App" style={{ backgroundImage: `url(${background})` }}>
       <h1>this is the home page</h1>
       <BrowserRouter>
         <Switch>
           <Route exact path="/">
+            <Auth currentUser={currentUser} setCurrentUser={setCurrentUser} />
             <Footer />
           </Route>
         </Switch>
---
+++ src/views/Auth/Auth.js
@@ -0,0 +1,38 @@
+import { useState } from 'react';
+import { signInUser, signUpUser } from '../services/users';
+import Header from '../components/Header/Header';
+import AuthForm from '../components/AuthForm/AuthForm';
+
+export default function Auth({ currentUser, setCurrentUser }) {
+  const [type, setType] = useState('signin');
+  const [errorMessage, setErrorMessage] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response =
+        type === 'signin' ? await signInUser(email, password) : await signUpUser(email, password);
+      setCurrentUser(response);
+    } catch {
+      setErrorMessage(
+        'Something went wrong. Please make sure all feilds are filled out correctly.'
+      );
+    }
+  };
+
+  return (
+    <>
+      <Header />
+      <AuthForm
+        email={email}
+        setEmail={setEmail}
+        password={password}
+        setPassword={setPassword}
+        handleSubmit={handleSubmit}
+        errorMessage={errorMessage}
+      />
+    </>
+  );
+}
