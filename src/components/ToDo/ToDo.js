ef005d9b8c911c5d6173c77ca950ac976c9352f9
Bailey Kuebelbeck <bailey.kue@gmail.com>
2022-01-11 16:13:57 -0800

    mapping through the todos to display them on the page

--- src/components/ToDo/ToDo.js
+++ src/components/ToDo/ToDo.js
@@ -1,10 +1,17 @@
 import './ToDo.css';
 
 export default function ToDo({ toDos }) {
-  console.log(toDos);
   return (
     <div>
       <h3>these will be your to dos</h3>
+      <ul>
+        {toDos.map((toDo) => (
+          <li key={toDo.id}>
+            <input type="checkbox" checked={toDo.is_complete} />
+            {toDo.task}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
